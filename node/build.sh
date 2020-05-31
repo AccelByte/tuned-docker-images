f45791255ea08c7aaa283bf3ac36a9aaf9d19e36
aris <aris@accelbyte.net>
2020-05-31 14:01:06 +0700

    update build base image node

--- node/build.sh
+++ node/build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [[ $# -ne 2 ]]; then
-  echo "This script needs base image and node version as arguments"
-  echo "example : ./build.sh alpine 8.12.0"
-  echo "example : ./build.sh debian 8.12.0"
+if [[ $# -ne 3 ]]; then
+  echo "This script needs base image, base image version, and node version as arguments"
+  echo "example : ./build.sh alpine 3.11 10.15.3"
+  echo "example : ./build.sh debian 3.11 10.15.3"
   exit -1
 fi
 
-sed "s/<nodeversion>/${2}/g" Dockerfile.${1} > Dockerfile
-docker build -t accelbyte/node:${2}-${1} .
-docker tag accelbyte/node:${2}-${1} accelbyte/node:latest
+sed -e "s/<nodeversion>/${3}/g" -e "s/<base_image_version>/${2}/g" Dockerfile.${1} > Dockerfile
+docker build -t accelbyte/node:${3}-${1}-${2} .
+docker tag accelbyte/node:${3}-${1}-${2} accelbyte/node:latest
 
 rm -rf Dockerfile
