4ae48eb5d2b1187f2533cfd17f9131e700222fc7
aris <aris@accelbyte.net>
2020-06-11 21:09:06 +0700

    update docker golang golang-builder

--- golang-builder/build.sh
+++ golang-builder/build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 if [[ $# -eq 0 ]]; then
-  echo "This script needs version as argument"
-  echo "example : ./build.sh 1.11.1"
+  echo "This script needs goversion and alpine version as arguments"
+  echo "example : ./build.sh 1.13 3.11"
   exit -1
 fi
 
-sed -i "s/<goversion>/${1}/g" Dockerfile
-docker build -t accelbyte/golang-builder:${1} .
-docker tag accelbyte/golang-builder:${1} accelbyte/golang-builder:latest
-
-sed -i "s/${1}/<goversion>/g" Dockerfile
+sed -e "s/<goversion>/${1}/g" -e "s/<base_image_version>/${2}/g" Dockerfile.template > Dockerfile
+docker build -t accelbyte/golang-builder:${1}-alpine${2} .
+docker tag accelbyte/golang-builder:${1}-alpine${2} accelbyte/golang-builder:latest
