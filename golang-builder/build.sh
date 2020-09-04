43d961fe7b5f34622d0f8f2b4b4b4f9fdfc72ea0
nyomanantonsuryawan <antonsuryawan@accelbyte.net>
2020-09-04 15:30:22 +0800

    update docker golang-builder
    - move alpine3.9's mongodb installation into another layer
    - update script to be able to define version with default value

--- golang-builder/build.sh
+++ golang-builder/build.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-if [[ $# -eq 0 ]]; then
-  echo "This script needs goversion and alpine version as arguments"
-  echo "example : ./build.sh 1.13 3.11"
-  exit -1
-fi
+goversion='1.14'
+base_image_version='3.12'
 
-sed -e "s/<goversion>/${1}/g" -e "s/<base_image_version>/${2}/g" Dockerfile.template > Dockerfile
-docker build -t accelbyte/golang-builder:${1}-alpine${2} .
-docker tag accelbyte/golang-builder:${1}-alpine${2} accelbyte/golang-builder:latest
+while getopts g:b:h option
+do
+	case "${option}"
+		in
+		g) goversion=${OPTARG};;
+		b) base_image_version=${OPTARG};;
+		h) echo "script usage: $(basename $0) [-h] [-g goversion-default-1.14] [-b base-image-version-default-is-3.12]" >&2 ; exit 1 ;;
+	esac
+done
+
+sed -e "s/<goversion>/${goversion}/g" -e "s/<base_image_version>/${base_image_version}/g" Dockerfile.template > Dockerfile
+docker build -t accelbyte/golang-builder:${goversion}-alpine${base_image_version} .
+docker tag accelbyte/golang-builder:${goversion}-alpine${base_image_version} accelbyte/golang-builder:latest
+
