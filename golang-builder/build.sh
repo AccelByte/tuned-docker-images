8c304d534f35aaa826cd937db30bf45b9b10d81f
antonsuryawan <antonsuryawan@gmail.com>
2020-09-09 12:18:06 +0800

    update indentation on remaining files

--- golang-builder/build.sh
+++ golang-builder/build.sh
@@ -4,12 +4,12 @@ base_image_version='3.12'
 
 while getopts g:b:h option
 do
-	case "${option}"
-		in
-		g) goversion=${OPTARG};;
-		b) base_image_version=${OPTARG};;
-		h) echo "script usage: $(basename $0) [-h] [-g goversion-default-1.14] [-b base-image-version-default-is-3.12]" >&2 ; exit 1 ;;
-	esac
+  case "${option}"
+    in
+    g) goversion=${OPTARG};;
+    b) base_image_version=${OPTARG};;
+    h) echo "script usage: $(basename $0) [-h] [-g goversion-default-1.14] [-b base-image-version-default-is-3.12]" >&2 ; exit 1 ;;
+  esac
 done
 
 sed -e "s/<goversion>/${goversion}/g" -e "s/<base_image_version>/${base_image_version}/g" Dockerfile.template > Dockerfile
--- golang/build.sh
+++ golang/build.sh
@@ -5,13 +5,13 @@ base_image_version='3.12'
 
 while getopts g:l:b:h option
 do
-	case "${option}"
-		in
-		g) goversion=${OPTARG};;
-		l) linterversion=${OPTARG};;
-		b) base_image_version=${OPTARG};;
-		h) echo "script usage: $(basename $0) [-h] [-g goversion-default-1.14] [-l linterversion-default-1.30.0] [-b base-image-version-default-is-3.12]" >&2 ; exit 1 ;;
-	esac
+  case "${option}"
+    in
+    g) goversion=${OPTARG};;
+    l) linterversion=${OPTARG};;
+    b) base_image_version=${OPTARG};;
+    h) echo "script usage: $(basename $0) [-h] [-g goversion-default-1.14] [-l linterversion-default-1.30.0] [-b base-image-version-default-is-3.12]" >&2 ; exit 1 ;;
+  esac
 done
 
 sed -e "s/<goversion>/${goversion}/g" \
